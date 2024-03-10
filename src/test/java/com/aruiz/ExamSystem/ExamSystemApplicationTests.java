5f41a2641819259330726be87e69dbb177bc978c
Antonio Ruiz <3speedemon3@gmail.com>
2024-03-10 17:48:15 +0100

    first commit

---
+++ src/test/java/com/aruiz/ExamSystem/ExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aruiz.ExamSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
